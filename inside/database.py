e4b624fb729d8dde3803e2bec5190d2cae72e902
insolor <insolor@gmail.com>
2022-07-05 21:19:17 +0300

    Change database url key name

--- inside/database.py
+++ inside/database.py
@@ -3,15 +3,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = config("SQLALCHEMY_DATABASE_URL")
+DATABASE_URL = config("DATABASE_URL")
 
 connect_args = (
     {"check_same_thread": False}
-    if SQLALCHEMY_DATABASE_URL.startswith("sqlite:")
+    if DATABASE_URL.startswith("sqlite:")
     else dict()
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
