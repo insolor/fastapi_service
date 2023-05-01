5d2ffce5eabb32b2f45ec021a33f2f92fe42c6ff
insolor <insolor@gmail.com>
2023-05-01 10:31:51 +0000

    Change declarative_base to DeclarativeBase (sqlalchemy 2.*)

--- inside/db_models.py
+++ inside/db_models.py
@@ -1,8 +1,9 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import DeclarativeBase, relationship
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 class User(Base):
